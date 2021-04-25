8a2076da5481472e7ccf79748307059754eba083
t18b219k <t18b219k@mail.cc.niigata-u.ac.jp>
2021-04-25 12:32:19 +0900

    engines rust fixing

--- src/engines.rs
+++ src/engines.rs
@@ -6,12 +6,12 @@ pub type InnerStatics = HashMap<String, isize>;
 
 pub fn engine_kill_self(texts: &[String], from: usize) -> InnerStatics {
     lazy_static! {
-        static ref re: Regex = Regex::new(r"\t(?P<name>[^が]+?)を撃破した").unwrap();
+        static ref RE: Regex = Regex::new(r"\t(?P<name>[^が]+?)を撃破した").unwrap();
     }
     let mut table = HashMap::new();
     let last = texts.len();
     for text in &texts[from..last]{
-        if let Some(caps) = re.captures(text) {
+        if let Some(caps) = RE.captures(text) {
             let name = caps.name("name").unwrap().as_str();
             match table.get_mut(name) {
                 None => {
@@ -23,25 +23,17 @@ pub fn engine_kill_self(texts: &[String], from: usize) -> InnerStatics {
             }
         }
     }
-    /*
-    let last = texts.len();
-    for i in from..last {
-        let text = &texts[i];
-
-    }
-    */
-
     table
 }
 
 pub fn engine_gacha(texts: &[String], from: usize) -> InnerStatics {
     lazy_static! {
-        static ref re: Regex = Regex::new(r"\[(?P<name>.+)] が当たりました！").unwrap();
+        static ref RE: Regex = Regex::new(r"\[(?P<name>.+)] が当たりました！").unwrap();
     }
     let mut table = HashMap::new();
     let last = texts.len();
     for text in &texts[from..last]{
-        if let Some(caps) = re.captures(&text) {
+        if let Some(caps) = RE.captures(&text) {
             let name = caps.name("name").unwrap().as_str();
             match table.get_mut(name) {
                 None => {
@@ -60,13 +52,12 @@ pub fn engine_gacha(texts: &[String], from: usize) -> InnerStatics {
 pub fn engine_item_use(texts: &[String], from: usize) -> InnerStatics {
     //[リペアパック2000] を使用した！
     lazy_static! {
-        static ref re: Regex = Regex::new(r"(?P<name>\[.+]) を使用した！").unwrap();
+        static ref RE: Regex = Regex::new(r"(?P<name>\[.+]) を使用した！").unwrap();
     }
     let mut table = HashMap::new();
     let last = texts.len();
-    for i in from..last {
-        let text = &texts[i];
-        if let Some(caps) = re.captures(&text) {
+    for text in &texts[from..last]{
+        if let Some(caps) = RE.captures(&text) {
             let name = caps.name("name").unwrap().as_str();
             match table.get_mut(name) {
                 None => {
@@ -92,8 +83,7 @@ pub fn engine_reward_dungeon(texts: &[String], from: usize) -> HashMap<String, (
     }
     let mut table: HashMap<String, (isize, isize)> = HashMap::new();
     let last = texts.len();
-    for i in from..last {
-        let text = &texts[i];
+    for text in &texts[from..last]{
         match RE2.captures(&text) {
             Some(caps) => {
                 let name = caps.name("name").unwrap().as_str();
@@ -141,14 +131,13 @@ pub fn engine_reward_dungeon(texts: &[String], from: usize) -> HashMap<String, (
 
 pub fn engine_rare(texts: &[String], from: usize) -> InnerStatics {
     lazy_static! {
-        static ref re: Regex =
+        static ref RE: Regex =
             Regex::new(r"誰かが \[(?P<name>.+)] をガチャセンターで当てました！").unwrap();
     }
     let mut table = HashMap::new();
     let last = texts.len();
-    for i in from..last {
-        let text = &texts[i];
-        if let Some(caps) = re.captures(&text) {
+    for text in &texts[from..last]{
+        if let Some(caps) = RE.captures(&text) {
             let name = caps.name("name").unwrap().as_str();
             match table.get_mut(name) {
                 None => {
@@ -169,17 +158,16 @@ pub fn engine_labo(texts: &[String], from: usize) -> InnerStatics {
     //1個でも合成に成功したら
     // (?P<name>) × \d+ の作成に成功しました。有機的な破片 × 4
     lazy_static! {
-    static ref re:Regex = Regex::new(r"(?P<name>.+) × (?P<N>.+)").unwrap();
+    static ref RE:Regex = Regex::new(r"(?P<name>.+) × (?P<N>.+)").unwrap();
     //先ずはre0でマッチさせてそして新旧判定とHashTableへの登録を行う
-    static ref re0:Regex = Regex::new(r"(?P<name>.+) の作成に成功しました。").unwrap();
+    static ref RE0:Regex = Regex::new(r"(?P<name>.+) の作成に成功しました。").unwrap();
         }
     let mut table = HashMap::new();
     let last = texts.len();
-    for i in from..last {
-        let text = &texts[i];
-        if let Some(caps) = re0.captures(text) {
+    for text in &texts[from..last]{
+        if let Some(caps) = RE0.captures(text) {
             let name = caps.name("name").unwrap().as_str();
-            let caps = re.captures(name);
+            let caps = RE.captures(name);
             let (k, val) = match caps {
                 None => {
                     //古いバージョンのログ
@@ -203,7 +191,6 @@ pub fn engine_labo(texts: &[String], from: usize) -> InnerStatics {
             }
         }
     }
-
     table
 }
 
@@ -214,29 +201,17 @@ pub fn engine_tsv_match(
 ) -> InnerStatics {
     let mut table = HashMap::new();
     let last = texts.len();
-    for i in from..last {
-        let mut text = texts[i].clone();
+    for text in &texts[from..last]{
+        let mut text =text.clone();
         text = text.replace("\r", "");
         text = text.replace("\t", "\\t");
         text.remove(0);
         text.remove(0);
         let name = dictionary.get(&text);
-        /*
-        match name {
-            None => {}
-            Some(name) => {
-                let qty = table.get(name).unwrap_or(&0);
-                table.insert(name.to_string(), qty + 1);
-            }
-        }*/
-
-        match name {
-            None=>{}
-            Some(name)=>{
-                match table.get_mut(name) {
-                    None => {table.insert(name.to_string(),1);}
-                    Some(qty) => {*qty+=1;}
-                }
+        if let Some(name) = name {
+            match table.get_mut(name) {
+                None => { table.insert(name.to_string(), 1); }
+                Some(qty) => { *qty += 1; }
             }
         }
     }
@@ -245,14 +220,12 @@ pub fn engine_tsv_match(
 
 pub fn engine_item_get(texts: &[String], from: usize) -> InnerStatics {
     lazy_static! {
-        static ref re: Regex = Regex::new(r"(?P<name>\[.+]) を (?P<N>\d+)個 取得した！").unwrap();
+        static ref RE: Regex = Regex::new(r"(?P<name>\[.+]) を (?P<N>\d+)個 取得した！").unwrap();
     }
     let mut table = HashMap::new();
     let last = texts.len();
-    for i in from..last {
-        let text = &texts[i];
-        println!("{}", text);
-        if let Some(caps) = re.captures(&text) {
+    for text in &texts[from..last]{
+        if let Some(caps) = RE.captures(&text) {
             let name = caps.name("name").unwrap().as_str();
             let num = caps.name("N").unwrap().as_str().parse::<isize>().unwrap();
             match table.get_mut(name) {
@@ -270,13 +243,12 @@ pub fn engine_item_get(texts: &[String], from: usize) -> InnerStatics {
 
 pub fn engine_get_part(texts: &[String], from: usize) -> InnerStatics {
     lazy_static! {
-        static ref re: Regex = Regex::new(r"(?P<name>\[.+]) を取得した！").unwrap();
+        static ref RE: Regex = Regex::new(r"(?P<name>\[.+]) を取得した！").unwrap();
     }
     let mut table = HashMap::new();
     let last = texts.len();
-    for i in from..last {
-        let text = &texts[i];
-        if let Some(caps) = re.captures(&text) {
+    for text in &texts[from..last]{
+        if let Some(caps) = RE.captures(&text) {
             let name = caps.name("name").unwrap().as_str();
             match table.get_mut(name) {
                 None => {
@@ -297,11 +269,11 @@ pub fn search_floor(texts: &[String], search_from: usize) -> Option<usize> {
     let mut floor = None;
     /*
     lazy_static! {
-        static ref re: Regex = Regex::new(r"(?P<name>.+?)がフロアゲートを起動した！").unwrap();
+        static ref RE: Regex = Regex::new(r"(?P<name>.+?)がフロアゲートを起動した！").unwrap();
     }
     for i in search_from..last {
         let text = &texts[i];
-        match re.captures(text) {
+        match RE.captures(text) {
             None => {}
             Some(_) => {
                 floor = i;
@@ -328,17 +300,12 @@ pub fn search_floor(texts: &[String], search_from: usize) -> Option<usize> {
 //フロアゲートの起動を探す.(first)
 pub fn search_floor_first(texts: &[String], search_from: usize) -> Option<usize> {
     let last = texts.len();
-    let floor = 0;
     lazy_static! {
-        static ref re: Regex = Regex::new(r"(?P<name>.+?)がフロアゲートを起動した！").unwrap();
+        static ref RE: Regex = Regex::new(r"(?P<name>.+?)がフロアゲートを起動した！").unwrap();
     }
-    for i in search_from..last {
-        let text = &texts[i];
-        match re.captures(text) {
-            None => {}
-            Some(_) => {
-                return Some(floor);
-            }
+    for (offset,text) in texts[search_from..last].iter().enumerate(){
+        if RE.captures(text).is_some(){
+            return Some(offset+search_from)
         }
     }
     None
@@ -348,21 +315,6 @@ pub fn search_floor_first(texts: &[String], search_from: usize) -> Option<usize>
 pub fn search_dungeon_clear(texts: &[String], search_from: usize) -> Option<usize> {
     //ダンジョン成功報酬
     let last = texts.len();
-    /*
-
-    lazy_static! {
-        static ref re: Regex = Regex::new(r"ダンジョン成功報酬").unwrap();
-    }
-    for i in search_from..last {
-        let text = &texts[i];
-        match re.captures(text) {
-            None => {}
-            Some(_) => {
-                return Some(i);
-            }
-        }
-    }
-    */
     for (offset,text) in texts[search_from..last].iter().enumerate(){
         if text.contains(r"ダンジョン成功報酬"){
             return Some(search_from+offset)
@@ -374,10 +326,10 @@ pub fn search_dungeon_clear(texts: &[String], search_from: usize) -> Option<usiz
 pub fn engine_get_text(text: &str) -> Vec<String> {
     let mut texts = vec![];
     lazy_static! {
-        static ref re: Regex =
+        static ref RE: Regex =
             Regex::new(r"\d{4}-\d{2}-\d{2}	\d{2}:\d{2}:\d{2}	\[INFO]	(?P<text>.+)").unwrap();
     }
-    for caps in re.captures_iter(text) {
+    for caps in RE.captures_iter(text) {
         texts.push("\t".to_string() + caps.name("text").unwrap().as_str());
     }
     texts
@@ -386,10 +338,10 @@ pub fn engine_get_text(text: &str) -> Vec<String> {
 pub fn engine_get_text3(text: &str) -> Vec<String> {
     let mut texts = vec![];
     lazy_static! {
-        static ref re: Regex =
+        static ref RE: Regex =
             Regex::new(r"\d{4}-\d{2}-\d{2}	\d{2}:\d{2}:\d{2}	(?P<text>.+)").unwrap();
     }
-    for caps in re.captures_iter(text) {
+    for caps in RE.captures_iter(text) {
         texts.push("\t".to_string() + caps.name("text").unwrap().as_str());
     }
     texts
@@ -400,10 +352,10 @@ pub fn engine_get_text2(text: &str) -> Vec<String> {
     let longtext;
     //\r\nを削除する代わりに<ls>をログの区切りとする.
     lazy_static! {
-        static ref re: Regex = Regex::new(r"(?P<time>\d{4}-\d{2}-\d{2}	\d{2}:\d{2}:\d{2})").unwrap();
+        static ref RE: Regex = Regex::new(r"(?P<time>\d{4}-\d{2}-\d{2}	\d{2}:\d{2}:\d{2})").unwrap();
     }
     //<ls>\date\time text
-    longtext = re
+    longtext = RE
         .replace_all(text, |caps: &Captures| {
             format!("<ls>{}", caps.name("time").unwrap().as_str())
         })
@@ -419,12 +371,12 @@ pub fn engine_get_text2(text: &str) -> Vec<String> {
 pub fn engine_get_info(texts: Vec<String>) -> Vec<String> {
     let mut vec = Vec::new();
     lazy_static! {
-        static ref re: Regex =
+        static ref RE: Regex =
             Regex::new(r"(?P<time>\d{4}-\d{2}-\d{2}	\d{2}:\d{2}:\d{2})\t\[INFO]\t(?P<text>.+)")
                 .unwrap();
     }
     for text in texts {
-        match re.captures(text.as_str()) {
+        match RE.captures(text.as_str()) {
             None => {}
             Some(caps) => vec.push(caps.name("text").unwrap().as_str().to_string()),
         }
